ef64470d733a76a1e8b0fe610670f701aefce5ca
Bradley Watkins <56637748+Littlewoko@users.noreply.github.com>
2021-11-22 11:01:00 +0000

    Added "main" layout file for web app
    use of name "newapp" due to app already existing

---
+++ resources/views/layouts/newapp.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="eng">
+    <head>
+        <title>
+            CSC348 Coursework -@yield('title')
+        </title>
+    </head>
+
+    <body>
+        <h1> 
+            CSC348 Coursework -@yield('title')
+        </h1>
+
+        <div>
+            @yield('content')
+        </div>
+
+</body>

441cb03be58032df62d6e9f17bb272cddf89074e
Bradley Watkins <56637748+Littlewoko@users.noreply.github.com>
2021-11-22 15:07:55 +0000

    error messages flashed on consumer creation

--- resources/views/layouts/newapp.blade.php
+++ resources/views/layouts/newapp.blade.php
@@ -11,8 +11,23 @@
             CSC348 Coursework -@yield('title')
         </h1>
 
+        @if ($errors->any())
+
+            <div>
+                Errors:
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>
+                            {{ $error }}
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+
+        @endif
+        
         <div>
             @yield('content')
         </div>
 
-</body>
+    </body>

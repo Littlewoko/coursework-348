9bd9e5db00c8178c673f0a85e69037ca4f2a783b
Bradley Watkins <56637748+Littlewoko@users.noreply.github.com>
2021-12-14 22:37:12 +0000

    Image model and database migration created

---
+++ database/migrations/2021_12_14_223042_create_images_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('faile_path');
+            $table->bigInteger('consumer_id')->unsigned();
+
+            $table->foreign('consumer_id')->references('id')
+            ->on('consumers')->onDelete('cascade')->onUpdate('cascade');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
